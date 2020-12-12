b7dd5974fe2898c39babd6ac530b8061a437ba42
ravipathak527 <35974311+ravipathak527@users.noreply.github.com>
2020-12-12 10:46:12 -0500

    Update
    Update

---
+++ Main.py
@@ -0,0 +1,8 @@
+import datetime
+date = datetime.date.today()
+
+def moneyCollection():
+    amount = input("Please enter the amount: ")
+    print("Today ", date, "you collected: ", amount)
+
+moneyCollection()
