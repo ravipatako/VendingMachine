0aa2dd7b46fcaf04a25889ac4208faead39feaca
ravipathak527 <35974311+ravipathak527@users.noreply.github.com>
2020-12-13 16:45:10 -0500

    Update
    Update

--- Main.py
+++ Main.py
@@ -3,6 +3,6 @@ date = datetime.date.today()
 
 def moneyCollection():
     amount = input("Please enter the amount: ")
-    print("Today ", date, "you collected: ", amount)
+    print("Today", date.strftime("%x"), "you collected:", amount)
 
 moneyCollection()
